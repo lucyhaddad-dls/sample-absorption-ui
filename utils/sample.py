742025d49f4465a4aaf4deb7547bf4b359b58c42
lucy haddad <l.haddad@diamond.ac.uk>
2026-08-12 12:02:23 +0000

    move sample utils to own file

---
+++ utils/sample.py
@@ -0,0 +1,103 @@
+from sample_mass_calcs.xas_sample import XRaySample
+from typing import Literal
+
+class SampleBuilder:
+    """
+    Convienience class for holding sample-related properties.
+
+    Attrs:
+        sample_dict (dict): Dict. of sample parameters `{"name": {"val", "dtype"}}`
+        sample (XRaySample): Sample object.
+    
+    Methods:
+        make_sample(): Make `sample`.
+        on_change(key, value): Update `sample_dict`.
+    """
+    def __init__(self):
+    # setting all to str/None to make input text field making easier.
+        self.sample_dict = {
+                "formula": {"val": "Cu", "dtype": str},
+                "absorber": {"val": "Cu", "dtype": str},
+                "edge": {"val": "K", "dtype":str},
+                "density": {"val": None, "dtype":float},
+                "area": {"val": None, "dtype":float},
+                "mass": {"val": None, "dtype":float},
+                "thickness": {"val": None, "dtype":float},
+                "mu_total": {"val": "2.6", "dtype":float},
+                "mass_unit": {"val": "g", "dtype":str},
+                "length_unit": {"val": "cm", "dtype":str},
+                "energy_unit": {"val": "gev", "dtype":str}
+                }
+        
+        self.make_sample()
+
+    def sample_dict_to_kwargs(self)->dict:
+        kwargs = {}
+        for k, v in self.sample_dict.items():
+            if v["val"] is None or v["val"] == "None":
+                kwargs[k] = None
+            else:
+                kwargs[k] = v["dtype"](v["val"])
+        return kwargs
+
+    def make_sample(self):
+        """
+        Remake `sample` with current `sample_dict` items.
+        """
+        values = self.sample_dict_to_kwargs()
+        self.sample = XRaySample(**values)
+
+
+    def update_sample_dict(self):
+        for k in self.sample_dict.keys():
+            val = getattr(self.sample, k)
+            if val is None:
+                tmp = self.sample_dict[k]
+                tmp["val"] = None
+                self.sample_dict[k] = tmp
+            elif hasattr(val, "value"):
+                self.sample_dict[k]["val"] = str(val.value)
+            else:
+                self.sample_dict[k]["val"] = str(val)
+
+    def on_value_change(self, 
+                        name:str,
+                        value:str,
+                        remake_sample:bool=False):
+
+        dtype = self.sample_dict[name]["dtype"]
+
+        measurement = getattr(self.sample, name)
+
+        if hasattr(measurement, "unit"):
+            measurement.value = dtype(value)
+        else:
+            measurement = dtype(value)
+
+        setattr(self.sample, name, measurement)
+
+        self.update_sample_dict()
+
+        if remake_sample is True:
+            self.make_sample()
+    
+    def get_name_and_unit(self, name:str)\
+        ->tuple[str|None, str|None]:
+        """
+        Get value and unit from given a name.
+        
+        Returns
+            tuple (tuple): tuple containing:
+                value (str | None): Value of `name`.
+                unit (Unit | str | None): Unit of `name`.
+        """
+        measurement = getattr(self.sample, name)
+        if hasattr(measurement, "unit"):
+            unit = measurement.unit; value = measurement.value
+        else:
+            unit = None; value = measurement
+        return value, unit
+
+
+
+    
